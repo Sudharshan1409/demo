8781b4daaa94b13624758e9d94b2e17d846443ae
Prashanth HN <prashanth@prashanth.net>
2021-04-10 19:25:17 +0530

    cdk init with cognito

---
+++ lib/crt-backend-stack.ts
@@ -0,0 +1,27 @@
+import * as cdk from '@aws-cdk/core';
+import * as cognito from '@aws-cdk/aws-cognito';
+
+
+export class CrtBackendStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new cognito.UserPool(this, 'myuserpool', {
+      userPoolName: 'platformsuite-userpool',
+      signInAliases: {
+        email: true,
+      },
+      autoVerify: {
+        email: true
+      },
+      passwordPolicy: {
+        minLength: 6,
+        requireLowercase: false,
+        requireUppercase: false,
+        requireDigits: false,
+        requireSymbols: false,
+      }
+    });
+  }
+}
